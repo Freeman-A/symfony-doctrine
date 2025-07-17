29a7785092edcd04a51f27e43f180d849bbdd52f
Freeman-A <alfye123@hotmail.com>
2025-07-17 12:14:39 +0100

    feature/added entitymanager to build DQL query rather than using direct repository access. removed the model because were now using database and doctrine to map entities

--- start/src/Controller/StarshipController.php
+++ start/src/Controller/StarshipController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Starship;
 use App\Model\StarshipRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class StarshipController extends AbstractController
 {
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
     #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
-    public function show(int $id, StarshipRepository $repository): Response
+    public function show(int $id, EntityManagerInterface $em): Response
     {
-        $ship = $repository->find($id);
+        $ship = $em->find(Starship::class, $id);
         if (!$ship) {
             throw $this->createNotFoundException('Starship not found');
         }
